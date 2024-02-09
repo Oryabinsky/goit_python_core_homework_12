da364363acc0520a1049e6fc3b4ad7ba56a87e86
Mike Oryabinsky <152095202+oryabinsky@users.noreply.github.com>
2024-02-10 01:05:40 +0200

    solved

--- bot.py
+++ bot.py
@@ -1,89 +1,106 @@
 """Console bot helper"""
 
-from classes import AddressBook, Record, Name, Phone
-
-# phonebook
-contacts = {}
+from classes import ObjectValidateError, AddressBook, Record
 
 
-def input_error(func):
+def handle_error(func):
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
+        except ObjectValidateError as e:
+            return e
         except KeyError:
             return 'Enter user name'
         except ValueError:
             return 'Give me name and phone please'
         except IndexError:
             return 'Invalid command format'
+        except PermissionError as e:
+            return f'No access rights! {e}'
     return wrapper
 
 
-def handle_invalid_command(*_):
+def handle_invalid_command(*args):
     return 'Invalid command format'
 
 
-def handle_hello(*_):
+def handle_hello(*args):
     return 'How can I help you?'
 
 
-def handle_end(*_):
+def handle_end(*args):
     return 'Good bye!'
 
 
-@input_error
+@handle_error
 def handle_contact_add(command):
-    name, phone = command
-    if name not in contacts:
-        contacts[name] = phone
-        return f'Contact {name} added with phone number {phone}'
-    else:
-        return f'Contact {name} already exists!'
+    name, phone, *birthday = command
+    record = Record(name)
+
+    record.add_phone(phone)
+
+    if birthday:
+        record.set_birthday(str(birthday[0]))
+
+    address_book.add_record(record)
+
+    return f'Contact {name} added with phone number: {phone}'
 
 
-@input_error
+@handle_error
 def handle_contact_change(command):
-    name, phone = command
-    if contacts.get(name):
-        contacts[name] = phone
-        return f'Phone number for {name} changed to {phone}'
-    else:
+    name, old_phone, new_phone = command
+    record = address_book.find(name)
+
+    if not record:
         return f'Contact {name} not found'
 
+    record.edit_phone(old_phone, new_phone)
 
-@input_error
+    return f'Contact {name}. Phone number {old_phone} changed to {new_phone}'
+
+
+@handle_error
 def handle_contact_get_by_name(command):
     name = command[0]
-    phone = contacts.get(name)
-    if phone:
-        return f'Phone number for {name} is {phone}'
-    else:
+    record = address_book.find(name)
+    if record:
+        return str(record)
+
+    return f'Contact {name} not found'
+
+
+@handle_error
+def handle_contact_search(command):
+    query = command[0]
+    records = address_book.search_full(query)
+
+    if records:
+        return f'Founded contacts:\n' + '\n'.join([str(record) for record in records])
+
+    return f'No contacts found for the request "{query}"'
+
+
+@handle_error
+def handle_contact_set_birthday(command):
+    name, birthday = command
+    record = address_book.find(name)
+
+    if not record:
         return f'Contact {name} not found'
 
+    record.set_birthday(birthday)
+    return f'Birthday {birthday} added for contact {name}'
 
-def handle_contact_get_all(*_):
-    if not contacts:
+
+def handle_contact_get_all(*args):
+    if not address_book.data:
         return 'No contacts found'
 
-    result = '{}{:<26}: {:<15}\n'.format(' ' * 3, 'Name', 'Phone number')
-    result += '\n'.join(
-        '{}{:<26}: {:<15}'.format(' ' * 3, name, phone)
-        for name, phone in contacts.items()
-    )
-
+    result = ''
+    for page in address_book.iterator():
+        result += '\n'.join(str(record) for record in page) + '\n'
     return result
-
-
-command_handlers = {
-    'hello': handle_hello,
-    'good bye': handle_end,
-    'close': handle_end,
-    'exit': handle_end,
-    'add': handle_contact_add,
-    'change': handle_contact_change,
-    'phone': handle_contact_get_by_name,
-    'show all': handle_contact_get_all
-}
 
 
 def get_handler(command: str) -> tuple:
@@ -112,23 +129,44 @@ def get_handler(command: str) -> tuple:
         else (handle_invalid_command, None)
 
 
+command_handlers = {
+    'hello': handle_hello,
+    'good bye': handle_end,
+    'close': handle_end,
+    'exit': handle_end,
+    'add': handle_contact_add,
+    'change': handle_contact_change,
+    'phone': handle_contact_get_by_name,
+    'birthday': handle_contact_set_birthday,
+    'search': handle_contact_search,
+    'show all': handle_contact_get_all
+}
+
+# initialize phonebook
+address_book = AddressBook()
+address_book.load_data_from_file()
+
+
 def main():
 
-    while True:
-        user_input = input('Enter command: ')
+    try:
+        while True:
+            user_input = input('Enter command: ')
 
-        if not user_input:
-            print(handle_invalid_command())
-            continue
+            if not user_input:
+                print(handle_invalid_command())
+                continue
 
-        handler, user_command_data = get_handler(user_input)
+            handler, user_command_data = get_handler(user_input)
 
-        answer = handler(user_command_data)
+            answer = handler(user_command_data)
 
-        print(answer)
+            print(answer)
 
-        if answer == 'Good bye!':
-            break
+            if answer == 'Good bye!':
+                break
+    finally:
+        address_book.save_data_to_file()
 
 
 if __name__ == '__main__':
--- classes.py
+++ classes.py
@@ -1,11 +1,20 @@
 from datetime import datetime
 from collections import UserDict
+from itertools import islice
+import os
+import pickle
+
+
+class ObjectValidateError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
 
 
 class Field:
     def __init__(self, value):
-        if not self.is_valid(value):
-            raise ValueError(f"Invalid value: {value}")
+        valid, msg = self.is_valid(value)
+        if not valid:
+            raise ObjectValidateError(msg)
         self.__value = value
 
     @property
@@ -13,13 +22,14 @@ class Field:
         return self.__value
 
     @value.setter
-    def value(self, new_value):
-        if not self.is_valid(new_value):
-            raise ValueError(f"Invalid value: {new_value}")
-        self.__value = new_value
+    def value(self, value):
+        valid, msg = self.is_valid(value)
+        if not valid:
+            raise ObjectValidateError(msg)
+        self.__value = value
 
     def is_valid(self, new_value):
-        return True
+        return True, None
 
     def __eq__(self, other):
         return self.__value == other
@@ -32,7 +42,13 @@ class Field:
 
 
 class Name(Field):
-    pass
+    def __init__(self, name):
+        super().__init__(name)
+
+    def is_valid(self, name):
+        if not name.isalpha():
+            return False, f'Invalid contact name "{name}"! Must be contain alpha letters'
+        return True, None
 
 
 class Phone(Field):
@@ -41,8 +57,8 @@ class Phone(Field):
 
     def is_valid(self, phone):
         if not phone.isdigit() or len(phone) != 10:
-            return False
-        return True
+            return False, f'Invalid phone number "{phone}"! Must be contain 10 digits only'
+        return True, None
 
 
 class Birthday(Field):
@@ -53,27 +69,25 @@ class Birthday(Field):
         try:
             datetime.strptime(birthday, "%d.%m.%Y")
         except ValueError:
-            raise ValueError('Invalid birthday format. Right birthday like this 28.12.1994')
+            return False, f'Invalid birthday "{birthday}". Right birthday forman d.m.y, like this 28.12.1994'
         else:
-            return True
+            return True, None
 
 
 class Record:
     def __init__(self, name, birthday=None):
         self.name = Name(name)
         self.phones = []
-        if birthday:
-            self.birthday = Birthday(birthday)
+        self.birthday = Birthday(birthday) if birthday else None
 
     def add_phone(self, phone):
-        if phone not in self._get_phones_list():
+        if phone not in self.get_phones_list():
             new_phone = Phone(phone)
             self.phones.append(new_phone)
 
     def remove_phone(self, phone):
-        if phone not in self._get_phones_list():
-            raise ValueError("Phone number not found")
-        self.phones.remove(phone)
+        phone_index = self.get_phones_list().index(phone)
+        del self.phones[phone_index]
 
     def edit_phone(self, old_phone, new_phone):
         self.remove_phone(old_phone)
@@ -84,8 +98,11 @@ class Record:
             if phone.value == desired_phone_number:
                 return phone
 
-    def _get_phones_list(self) -> list:
+    def get_phones_list(self) -> list:
         return [phone.value for phone in self.phones]
+
+    def set_birthday(self, birthday):
+        self.birthday = Birthday(birthday)
 
     def days_to_birthday(self):
         if not self.birthday:
@@ -98,72 +115,78 @@ class Record:
         return (next_birthday - now).days
 
     def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+        text_view = f"Contact name: {self.name.value}; phones: {', '.join(p.value for p in self.phones)}"
+        text_view += '; days to birth: ' + str(self.days_to_birthday()) if self.birthday else ''
+        return text_view
 
 
 class AddressBook(UserDict):
+    def __init__(self):
+        super().__init__()
+
+        self.data_file_name = 'address_book.bin'
+        self.current_directory = os.path.dirname(os.path.abspath(__file__))
+        self.data_file_path = os.path.join(self.current_directory, self.data_file_name)
+
+    def save_data_to_file(self):
+        if not os.access(self.current_directory, os.W_OK):
+            raise PermissionError(f"Cannot write to: {self.data_file_path}")
+
+        file_path = os.path.join(self.current_directory, self.data_file_name)
+        with open(file_path, 'wb') as file:
+            pickle.dump(self.data, file)
+
+    def load_data_from_file(self):
+        # check for file exists and read access rights
+        if os.path.exists(self.data_file_path) and os.access(self.current_directory, os.R_OK):
+            with open(self.data_file_path, 'rb') as file:
+                # check loading data
+                loaded_data = pickle.load(file)
+                for name, record in loaded_data.items():
+                    if isinstance(record, Record) and self.validate_record(record):
+                        self.add_record(record)
+                    else:
+                        # if at least one error is found, then return an empty AddressBook.
+                        self.data = {}
+                        return
+        else:
+            self.data = {}
+
+    @staticmethod
+    def validate_record(record):
+        if not isinstance(record.name, Name):
+            return False
+        for phone in record.phones:
+            if not isinstance(phone, Phone):
+                return False
+        if record.birthday:
+            if not isinstance(record.birthday, Birthday):
+                return False
+        return True
 
     def add_record(self, record):
+        if record.name.value in self.data:
+            raise ObjectValidateError(f'Contact with name {record.name.value} already exist. Try with another one')
         self.data[record.name.value] = record
-
-    def find(self, name):
-        return self.data.get(name)
 
     def delete(self, name):
         if name in self.data:
             del self.data[name]
 
-    def iterator(self, n=2):
-        start = 0
-        end = n
-        while start < len(self.data):
-            yield {name: self.data[name] for name in list(self.data)[start:end]}
-            start += n
-            end += n
+    def find(self, name):
+        return self.data.get(name, None)
 
+    def search_full(self, query):
+        found_records = []
+        query = query.lower().strip()
 
-# def main():
-#     # Створення нової адресної книги
-#     book = AddressBook()
-#
-#     # Створення запису для John
-#     john_record = Record("John", "19.10.2004")
-#     john_record.add_phone("1234567890")
-#     john_record.add_phone("5555555555")
-#     # john_record.add_phone("77777777777")  # не валідний номер телефону
-#     # john_record.add_phone("bd2345vxe")  # не валідний номер телефону
-#     print(f'print john_record {john_record}')
-#
-#     # Додавання запису John до адресної книги
-#     book.add_record(john_record)
-#
-#     # Створення та додавання нового запису для Jane
-#     jane_record = Record("Jane")
-#     jane_record.add_phone("9876543210")
-#     book.add_record(jane_record)
-#
-#     # Виведення всіх записів у книзі
-#     for book_record in book.data.values():
-#         print(str(book_record))
-#
-#     # Знаходження та редагування телефону для John
-#     john = book.find("John")
-#     if john:
-#         john.edit_phone("1234567890", "1112223333")
-#
-#     if john:
-#         print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
-#
-#     # Пошук конкретного телефону у записі John
-#     if john:
-#         found_phone = john.find_phone("5555555555")
-#         print(f"{john.name.value}: {found_phone}")  # Виведення: 5555555555
-#         found_phone = john.find_phone("1234567890")
-#         print(f"{john.name.value}: {found_phone}")  # Виведення: 5555555555
-#
-#     # Видалення запису Jane
-#     book.delete("Jane")
-#
-#
-# if __name__ == '__main__':
-#     main()
+        for record in self.data.values():
+            if query in record.name.value.lower() or query in ' '.join(record.get_phones_list()):
+                found_records.append(record)
+
+        return found_records
+
+    def iterator(self, page_size=10):
+        keys = list(self.data.keys())
+        for i in range(0, len(keys), page_size):
+            yield [self.data[key] for key in islice(keys, i, i + page_size)]
